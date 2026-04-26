7c9daa16e5e3cc4017d694a10d2e87cdba0f9d43
Crxtal <crxtal@hotmail.com>
2026-04-26 15:38:45 -0500

    Implement Express proxy server for Discord
    Replaced SOCKS server implementation with an Express proxy server to target Discord. Removed unused code and adjusted server setup.

--- index.js
+++ index.js
@@ -1,46 +1,69 @@
-const socks = require('socksv5');
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
-const http = require('http');
+const app = express();
 
+// The "Equation": Substitute /proxy with the target website
+app.use('/proxy', createProxyMiddleware({
+    target: 'https://discord.com', // Change this to the site you want to proxy
+    changeOrigin: true,
+    pathRewrite: {
+        '^/proxy': '', // Removes /proxy from the URL
+    },
+}));
 
-const srv = socks.createServer((info, accept, deny) => {
-
-    console.log(`[SOCKS] Connection to: ${info.dstAddr}`);
-
-    accept();
-
-});
-
-
-
-const httpServer = http.createServer((req, res) => {
-
-    if (req.url === '/' || req.url === '/ping') {
-
-        console.log(`[Uptime] Ping received at ${new Date().toLocaleTimeString()}`);
-
-        res.writeHead(200);
-
-        return res.end('Proxy is Awake');
-
-    }
-
-    res.writeHead(404);
-
-    res.end();
-
-});
-
-
-
+// Port binding is critical for Render
 const PORT = process.env.PORT || 3000;
-
-httpServer.listen(PORT, () => {
-
-    console.log(`Hybrid Server running on Port ${PORT}`);
-
+app.listen(PORT, () => {
+    console.log(`Proxy is running on port ${PORT}`);
 });
 
 
 
-srv.useAuth(socks.auth.None());
+
+// const socks = require('socksv5');
+
+// const http = require('http');
+
+
+// const srv = socks.createServer((info, accept, deny) => {
+
+//     console.log(`[SOCKS] Connection to: ${info.dstAddr}`);
+
+//     accept();
+
+// });
+
+
+
+// const httpServer = http.createServer((req, res) => {
+
+//     if (req.url === '/' || req.url === '/ping') {
+
+//         console.log(`[Uptime] Ping received at ${new Date().toLocaleTimeString()}`);
+
+//         res.writeHead(200);
+
+//         return res.end('Proxy is Awake');
+
+//     }
+
+//     res.writeHead(404);
+
+//     res.end();
+
+// });
+
+
+
+// const PORT = process.env.PORT || 3000;
+
+// httpServer.listen(PORT, () => {
+
+//     console.log(`Hybrid Server running on Port ${PORT}`);
+
+// });
+
+
+
+// srv.useAuth(socks.auth.None());
